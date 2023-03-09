e3b506748d3d01480b25f73d59eadc4e845b51fe
chlqls <chlqls212@gmail.com>
2023-03-09 23:35:49 +0900

    ✨ feat: solve Problem1

--- src/main/java/onboard/Problem1.java
+++ src/main/java/onboard/Problem1.java
@@ -6,6 +6,17 @@ import java.util.List;
 public class Problem1 {
     public static List<Integer> solution(Integer hour, Integer minute){
         List<Integer> answer = new ArrayList<>();
+        if (minute >= 45) {
+            minute -= 45;
+        }
+        else {
+            minute += 60 - 45;
+            hour -= 1;
+            if(hour<0) {
+                hour = 23;
+            }
+        }
+        answer = List.of(hour, minute);
         return answer;
     }
 }
