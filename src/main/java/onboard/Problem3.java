4a42171b8359723ee94aeb4fbb8692df4ad30711
chlqls <chlqls212@gmail.com>
2023-03-12 19:08:27 +0900

    ✨ feat: solve Problem3

--- src/main/java/onboard/Problem3.java
+++ src/main/java/onboard/Problem3.java
@@ -1,11 +1,32 @@
 package onboard;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem3 {
     public static List<Integer> solution(List<Integer> numbers){
         List<Integer> answer = new ArrayList<>();
+
+        int sum = 0;
+        for(int i = 0; i < 5; i++) {
+            sum += numbers.get(i);
+        }
+
+        Collections.sort(numbers);
+//        for(int i = 0; i < 4; i++) {
+//            for(int j = i + 1; j < 5; j++) {
+//                if(numbers.get(i) > numbers.get(j)){
+//                    Collections.swap(numbers, i, j);
+                    //int temp = numbers.get(i);
+                    //numbers.set(i, numbers.get(j));
+                    //numbers.set(j, temp);
+//                }
+//            }
+//        }
+
+        answer = List.of(sum / 5, numbers.get(2));
+
         return answer;
     }
 }
