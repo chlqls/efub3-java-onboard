004b325747b23f116942e9b1538d3826dd2d54e5
chlqls <chlqls212@gmail.com>
2023-03-12 20:24:01 +0900

    ✨ feat: solve Problem4

--- src/main/java/onboard/Problem4.java
+++ src/main/java/onboard/Problem4.java
@@ -1,12 +1,24 @@
 package onboard;
 
-/**
- * 리턴값 answer의 경우, 변수 초기값은 변경 가능합니다.
- * 다만 answer로 return하는 것은 변경하지 않도록 주의해주세요.
- */
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class Problem4 {
     public static Integer solution(Integer number){
         Integer answer = 0;
+
+        ArrayList<Integer> arrNum = new ArrayList<>();
+        while(number > 0) {
+            arrNum.add(number % 10);
+            number /= 10;
+        }
+        arrNum.sort(Comparator.naturalOrder());
+
+        for(int i = 0; i < arrNum.size(); i++) {
+            answer += (int)(arrNum.get(i) * Math.pow(10, i));
+        }
+
         return answer;
     }
 }
